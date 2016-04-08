c0aea86b3878a21010d7292cb2bc5676cc5c30da
brianscroggins24 <brian.scroggins24@gmail.com>
2016-04-08 09:17:38 -0500

    passing in props to SuperImage from instantiated components

--- kitchen-sink/client/imports/components/App.jsx
+++ kitchen-sink/client/imports/components/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { SuperImage } from 'react-super-components';
 
+const ImageLoading = () => (
+  <div className="LOADING">Loading component test</div>
+);
+
+const ImageError = () => (
+  <div className="ERROR">Error component test</div>
+);
+
 export default class App extends React.Component {
   render() {
     return (
       <SuperImage
         src="http://brentcarnduff.com/wp-content/uploads/2014/08/url-small.jpg"
-        style={{}} />
-    )
+        loadingComponent={<ImageLoading />}
+        errorComponent={<ImageError />}
+        style={{}}
+      />
+    );
   }
 }
 
