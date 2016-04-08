b622554cbf1c94d18ee7a61882e1003de62efd0a
martinbee <martin@poeticsystems.com>
2016-04-08 15:28:08 -0500

    set up boiler to test

--- kitchen-sink/client/imports/components/App.jsx
+++ kitchen-sink/client/imports/components/App.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { SuperImage } from 'react-super-components';
+import { VirtualScroll } from 'react-virtualized';
+import ReactList from 'react-list';
+import './styles.css'; // only needs to be imported once
+
+const list = Array.from(Array(1000).keys());
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      list: [],
+    };
+  }
+
   render() {
+    if (this.state.list === 'virtual') {
+      return (
+        <div id='virtual-scroll-wrapper'>
+          <button onClick={ () => { this.setState({ list: 'false' }) } }>react-list</button>
+          <VirtualScroll
+            width={320}
+            height={500}
+            rowsCount={list.length}
+            rowHeight={20}
+            rowRenderer={
+              index => list[index] // Could also be a DOM element
+            }
+          />
+        </div>
+      );
+    }
+
     return (
-      <SuperImage
-        src="http://brentcarnduff.com/wp-content/uploads/2014/08/url-small.jpg"
-        style={{}} />
-    )
+      <div id='virtual-scroll-wrapper' style={{ width: '320px', height: '500px', overflow: 'scroll' }}>
+        <button onClick={ () => { this.setState({ list: 'virtual' }) } }>virtual-list</button>
+        <ReactList
+          itemRenderer={
+            index => <div key={index}> {list[index]} </div>
+          }
+          length={list.length}
+          type='uniform'
+         />
+      </div>
+    );
   }
 }
 
