8ff1712441135917906bcbb05c58ee5367ea96de
Chun Yang <yang2007chun@gmail.com>
2016-04-13 14:43:48 -0500

    test

---
+++ test/lib/SuperImage.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import SuperImage from '../../lib/SuperImage'
+import {assert} from 'chai';
+
+describe('SuperImage', () => {
+  it('should render', () => {
+    assert(true);
+  });
+});
