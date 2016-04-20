96cabd6aa3e09818134966f091fffce4cc1ceec4
Chun Yang <yang2007chun@gmail.com>
2016-04-20 17:11:41 -0500

    add permanent subscriptions

--- kitchen-sink/client/imports/components/SubscriptionsDemo.jsx
+++ kitchen-sink/client/imports/components/SubscriptionsDemo.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import Cars from '/shared/cars';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
+import Subscriptions, { permanentlySubscribe } from '../lib/Subscriptions';
 
 export default class SubscriptionsDemo extends React.Component {
-  render () {
+  render() {
+    const CarsComponent = ({ cars }) => {
+      return (
+        <ul>
+          {
+            cars.map((car, index) => {
+              return <li key={index}>{car.name}</li>;
+            })
+          }
+        </ul>
+      );
+    };
+
+    const CarsContainer = createContainer(() => {
+      return {
+        cars: Cars.find().fetch()
+      };
+    }, CarsComponent);
+
     return (
-      <div> </div>
-    )
+      <div>
+        <h2>permanentlySubscribe</h2>
+        <Subscriptions
+          getSubscriptions={() => { return [permanentlySubscribe('cars')] }}
+          unsubscribe={false}
+          displayComponent={<CarsContainer/>} />
+        <h2>Meteor.subscribe (this subscribe is unsubscribed automatically)</h2>
+        <Subscriptions
+          getSubscriptions={() => { return [Meteor.subscribe('cars')] }}
+          unsubscribe={false}
+          displayComponent={<CarsContainer/>} />
+      </div>
+    );
   }
 }
