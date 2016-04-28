08b5bef0f3d13d58568be5c6de312a76f075789a
Chun Yang <yang2007chun@gmail.com>
2016-04-28 15:10:40 -0500

    update kitchen sink for ParamStore

--- kitchen-sink/client/imports/components/Index.jsx
+++ kitchen-sink/client/imports/components/Index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import ParamStore from 'param-store';
+import { Link } from 'param-store';
 
 export default class Index extends React.Component {
   render() {
     return (
       <ul>
-        <li onClick={() => {ParamStore.set({path: 'image'})}}>Image</li>
-        <li onClick={() => {ParamStore.set({path: 'subscription'})}}>Subscriptions</li>
-        <li onClick={() => {ParamStore.set({path: 'list'})}}>List</li>
-        <li onClick={() => {ParamStore.set({path: 'stack'})}}>Stack</li>
+        <li><Link params={{path: 'image'}}>Image</Link></li>
+        <li><Link params={{path: 'subscription'}}>Subscription</Link></li>
+        <li><Link params={{path: 'list'}}>List</Link></li>
+        <li><Link params={{path: 'stack'}}>Stack</Link></li>
       </ul>
     );
   }
