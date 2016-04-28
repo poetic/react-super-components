9302bd931b5690cd47a9930e53c7d2f1e53c6e3d
Chun Yang <yang2007chun@gmail.com>
2016-04-28 17:14:49 -0500

    refactor

--- kitchen-sink/client/imports/components/App.jsx
+++ kitchen-sink/client/imports/components/App.jsx
@@ -4,15 +4,18 @@ import ImageDemo from './ImageDemo';
 import SubscriptionsDemo from './SubscriptionsDemo';
 import ListDemo from './ListDemo';
 import StackDemo from './StackDemo';
-import Stack from '../lib/Stack'
-import Layer from '../lib/Layer'
+import Stack from '../lib/Stack';
+import Layer from '../lib/Layer';
+import Assembler from '../lib/Assembler';
 
 export default () => (
-  <Stack id='path'>
-    <Layer id='' component={Index} />
-    <Layer id='image' component={ImageDemo} />
-    <Layer id='subscription' component={SubscriptionsDemo} />
-    <Layer id='list' component={ListDemo} />
-    <Layer id='stack' component={StackDemo} />
-  </Stack>
+  <Assembler>
+    <Stack id='path'>
+      <Layer id='' component={Index} />
+      <Layer id='image' component={ImageDemo} />
+      <Layer id='subscription' component={SubscriptionsDemo} />
+      <Layer id='list' component={ListDemo} />
+      <Layer id='stack' component={StackDemo} />
+    </Stack>
+  </Assembler>
 );
