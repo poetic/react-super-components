1244350c2375eb485df4b6c3fedfcc737ba50015
Chun Yang <yang2007chun@gmail.com>
2016-05-02 12:20:37 -0500

    wrap layers in a div inside stack

--- kitchen-sink/client/imports/components/StackDemo.jsx
+++ kitchen-sink/client/imports/components/StackDemo.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
 import Stack from '../lib/Stack';
-import ParamStore from 'param-store';
+import ParamStore, {Link} from 'param-store';
 
 export default class StackDemo extends React.Component {
   render () {
     return (
       <div>
-        <h1>Stack Without animation (id: x)</h1>
+        <h1>Stack Without animation (index: no-animation)</h1>
         <ul>
-          <li><button onClick={() => {ParamStore.set({x: 'first'})}}>first</button></li>
-          <li><button onClick={() => {ParamStore.set({x: 'second'})}}>second</button></li>
-          <li><button onClick={() => {ParamStore.set({x: 'third'})}}>third</button></li>
+          <li><Link params={{'no-animation': 'first'}}>first</Link></li>
+          <li><Link params={{'no-animation': 'second'}}>second</Link></li>
+          <li><Link params={{'no-animation': 'third'}}>third</Link></li>
         </ul>
-        <Stack id='x' defaultActiveLayerId='first' style={{backgroundColor: 'yellow'}}>
-          <div id='first'>FIRST</div>
-          <div id='second'>SECOND</div>
-          <div id='third'>THIRD</div>
+        <Stack index='no-animation'
+          defaultActiveLayerIndex='first'
+          style={{backgroundColor: 'yellow'}}>
+          <div index='first'>FIRST</div>
+          <div index='second'>SECOND</div>
+          <div index='third'>THIRD</div>
+        </Stack>
+
+        <h1>Stack with opacity animation (index: animation)</h1>
+        <ul>
+          <li><Link params={{'animation': 'first'}}>first</Link></li>
+          <li><Link params={{'animation': 'second'}}>second</Link></li>
+          <li><Link params={{'animation': 'third'}}>third</Link></li>
+        </ul>
+        <Stack
+          index='animation'
+          style={{color: 'white'}}
+          defaultActiveLayerIndex='first'>
+          <div index='first' style={{backgroundColor: 'blue'}}>FIRST</div>
+          <div index='second' style={{backgroundColor: 'green'}}>SECOND</div>
+          <div index='third' style={{backgroundColor: 'red'}}>THIRD</div>
         </Stack>
       </div>
     )
