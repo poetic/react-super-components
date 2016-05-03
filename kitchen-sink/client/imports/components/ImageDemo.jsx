bfe52c577d4f32a9a516b1b74def65545e119b75
brianscroggins24 <brian.scroggins24@gmail.com>
2016-05-03 17:21:16 -0500

    added wrapperprops and imageprops to image component to pass down styles

--- kitchen-sink/client/imports/components/ImageDemo.jsx
+++ kitchen-sink/client/imports/components/ImageDemo.jsx
@@ -3,10 +3,20 @@ import Image from '../lib/Image';
 
 export default class ImageDemo extends React.Component {
   render() {
-    const style = {
-      width: '400px',
-      height: '300px',
-      objectFit: 'cover',
+
+    const imageProps = {
+      style: {
+        width: '400px',
+        height: '300px',
+        objectFit: 'cover',
+      },
+    };
+
+    const wrapperProps = {
+      style: {
+        width: '100%',
+        height: '100%',
+      },
     };
 
     return (
@@ -16,34 +26,35 @@ export default class ImageDemo extends React.Component {
         <h2>Default</h2>
         <Image
           src="http://trichilofoods.com/site/wp-content/uploads/2015/06/veggies.jpg"
-          style={style}
+          imageProps={imageProps}
+          wrapperProps={wrapperProps}
         />
 
         <h2>loadingSrc</h2>
         <Image
           src="http://trichilofoods.com/site/wp-content/uploads/2015/06/veggies.jpg"
           loadingSrc="http://www.jqueryscript.net/images/Minimal-jQuery-Loading-Overlay-Spinner-Plugin-Easy-Overlay.jpg"
-          style={style}
+          imageProps={imageProps}
         />
 
         <h2>LoadingComponent</h2>
         <Image
           src="http://trichilofoods.com/site/wp-content/uploads/2015/06/veggies.jpg"
           LoadingComponent={(props) => <div {...props}>LOADING...</div>}
-          style={style}
+          imageProps={imageProps}
         />
 
         <h2>Error (for now error is the same as loading)</h2>
         <Image
           src="http://nonexist-image.jpg"
-          style={style}
+          imageProps={imageProps}
         />
 
         <div style={{ height: '100vh' }}></div>
         <h2>Lazy loading by default</h2>
         <Image
           src="http://trichilofoods.com/site/wp-content/uploads/2015/06/veggies.jpg"
-          style={style}
+          imageProps={imageProps}
         />
 
       </div>
