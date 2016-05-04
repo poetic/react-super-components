37b44e175aebd881f4d4ed2bc1a343b7d7f902f2
brianscroggins24 <brian.scroggins24@gmail.com>
2016-05-04 12:09:34 -0500

    removed image props prop in favor of just flattening out all other props passed it

--- kitchen-sink/client/imports/components/ImageDemo.jsx
+++ kitchen-sink/client/imports/components/ImageDemo.jsx
@@ -3,13 +3,10 @@ import Image from '../lib/Image';
 
 export default class ImageDemo extends React.Component {
   render() {
-
-    const imageProps = {
-      style: {
-        width: '400px',
-        height: '300px',
-        objectFit: 'cover',
-      },
+    const style = {
+      width: '400px',
+      height: '300px',
+      objectFit: 'cover',
     };
 
     const wrapperProps = {
@@ -26,7 +23,7 @@ export default class ImageDemo extends React.Component {
         <h2>Default</h2>
         <Image
           src="http://trichilofoods.com/site/wp-content/uploads/2015/06/veggies.jpg"
-          imageProps={imageProps}
+          style={style}
           wrapperProps={wrapperProps}
         />
 
@@ -34,27 +31,27 @@ export default class ImageDemo extends React.Component {
         <Image
           src="http://trichilofoods.com/site/wp-content/uploads/2015/06/veggies.jpg"
           loadingSrc="http://www.jqueryscript.net/images/Minimal-jQuery-Loading-Overlay-Spinner-Plugin-Easy-Overlay.jpg"
-          imageProps={imageProps}
+          style={style}
         />
 
         <h2>LoadingComponent</h2>
         <Image
           src="http://trichilofoods.com/site/wp-content/uploads/2015/06/veggies.jpg"
           LoadingComponent={(props) => <div {...props}>LOADING...</div>}
-          imageProps={imageProps}
+          style={style}
         />
 
         <h2>Error (for now error is the same as loading)</h2>
         <Image
           src="http://nonexist-image.jpg"
-          imageProps={imageProps}
+          style={style}
         />
 
         <div style={{ height: '100vh' }}></div>
         <h2>Lazy loading by default</h2>
         <Image
           src="http://trichilofoods.com/site/wp-content/uploads/2015/06/veggies.jpg"
-          imageProps={imageProps}
+          style={style}
         />
 
       </div>
