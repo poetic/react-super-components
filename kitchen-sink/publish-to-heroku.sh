ea438dbb40e456e062e2acafc1a6c2bdaa4915b6
Chun Yang <yang2007chun@gmail.com>
2016-06-23 10:03:05 -0500

    fix: the publish issue

--- kitchen-sink/publish-to-heroku.sh
+++ kitchen-sink/publish-to-heroku.sh
@@ -1,10 +1,16 @@
+rm -rf .git
+
 rm client/imports/lib
 cp -r ../lib client/imports/
 
-rm -rf .git
 git init
 git add -A
 git commit -m 'init'
 
 heroku git:remote -a react-super-components
 git push heroku master --force &> /dev/null &
+
+rm -r client/imports/lib
+ln -s ../../../lib client/imports/lib
+
+rm -rf .git
